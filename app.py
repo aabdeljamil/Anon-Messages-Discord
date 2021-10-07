c669c406caf5675191aac5626a5cc4e5a5964b42
aabdeljamil <43014818+aabdeljamil@users.noreply.github.com>
2021-10-07 12:50:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 import discord
 
-TOKEN = ''
+TOKEN = 'bot_token_here'
 
 client = discord.Client(intents=discord.Intents.all())
 
 @client.event
 async def on_message(message):
-    id = #need channel id here
-    channelToSendTo = client.get_channel(id)
+    channelToSendTo = discord.utils.get(client.guilds[0].text_channels, name="replace_with_channel_name")
 
-    if message.author.bot:
+    if message.author.bot: #don't want bot to reply to itself
         return
 
-    if isinstance(message.channel, discord.DMChannel):
-        await channelToSendTo.send(message.content)
+    if isinstance(message.channel, discord.DMChannel): #message is a dm
+        await channelToSendTo.send(message.content) #send the same message to the specified channel on line 9
 
 
 @client.event
-async def on_ready():
+async def on_ready(): #ouput to the terminal when bot is ready
     for guild in client.guilds:
         print(client.user, 'connected to guild ', guild.name, '#', guild.id)
 
